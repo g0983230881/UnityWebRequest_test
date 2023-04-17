845614eba0f339ce9e0793ba385b2ec9874102ae
Jordan <g0983230881@gmail.com>
2023-04-17 18:32:11 +0800

    all info have added in dashboard

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/mongodbSchema_Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/webRequest_dashboard.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
